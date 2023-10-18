1fec8d65dc855864eb7f83d144c5640dd5e037c9
Emilio Fernández <115873637+Zninjavenomzz@users.noreply.github.com>
2023-10-18 10:15:43 +0200

    Add files via upload

---
+++ Actividad204a/src/es/iespablopicasso/programacion/unidad02/actividad0201/ContadorObjetos.java
@@ -0,0 +1,24 @@
+package es.iespablopicasso.programacion.unidad02.actividad0201;
+
+public class ContadorObjetos {
+
+	public static void main(String[] args) {
+		
+		GranHermano hermano1 = new GranHermano(12);
+		GranHermano hermano2 = new GranHermano(12);
+		GranHermano hermano3 = new GranHermano(12);
+		GranHermano hermano4 = new GranHermano(12);
+		
+		System.out.println("Se han creado " + GranHermano.getNumeroObjetosCreados() + " objetos");
+		
+		GranHermano hermano5 = new GranHermano(12);
+		GranHermano hermano6 = new GranHermano(12);
+		GranHermano hermano7 = new GranHermano(12);
+		GranHermano hermano8 = new GranHermano(12);
+		
+		
+		System.out.print("Se han creado " + GranHermano.getNumeroObjetosCreados() + " objetos");
+		
+	}
+
+}
